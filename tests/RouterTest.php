2feeca718e751ec310c087b5ca1bc3dccd506d6e
Mikushi <bartz.michel@gmail.com>
2012-01-14 18:40:03 +0100

    Adding Router Unit Test

---
+++ tests/RouterTest.php
@@ -0,0 +1,32 @@
+<?php
+include "../src/Tachyon/Router.php";
+class RouterTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @dataProvider provider
+	 */ 
+	public function testConstructor($uri, $method) {
+		$_SERVER['REQUEST_URI'] = $uri;
+		$_SERVER['REQUEST_METHOD'] = $method;
+		$urls = array(
+			"/" => "Index",
+			"/home" => "Home",
+			"/user/:id" => "User"
+		);
+		$router = new \Tachyon\Router($urls);
+		
+		$this->assertClassHasAttribute("notFound", "\\Tachyon\\Router");
+		$this->assertEquals(strtolower($method), $router->getMethod());
+		$this->assertContains($router->getController(), $urls);
+	    $this->assertInternalType("array", $router->getParams());	
+	}
+
+	public function provider() {
+		return array(
+			array("/","GET"),
+			array("/home","GET"),
+			array("/user/12","DELETE")
+		);
+	}
+}
+
