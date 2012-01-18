70506f2f7599e50953eca8bbc070cb0c8bce116b
Mikushi <bartz.michel@gmail.com>
2012-01-18 13:16:18 +0100

    Updating examples

--- examples/SimpleSite/controllers/Home.php
+++ examples/SimpleSite/controllers/Home.php
@@ -4,5 +4,7 @@ class Home extends \Tachyon\Controller
 	public function get() {
 		$this->name = htmlentities(stripslashes($this->getData("name","redditor")));
 		$this->render("index.tpl");
+
+		$this->sendResponse();
 	}
 }
--- examples/skeleton/controllers/Home.php
+++ examples/skeleton/controllers/Home.php
@@ -4,5 +4,6 @@ class Home extends \Tachyon\Controller
 	public function get() {
 		$this->content = "Lorem Ipsum";
 		$this->render("home/main.tpl");
+		$this->sendResponse();
 	}
 }
